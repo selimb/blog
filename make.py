911290522a5d72eef10c41806da1b66b3de646c2
Selim Belhaouane <selim.belhaouane@gmail.com>
2021-04-26 19:12:23 -0400

    fixes

--- make.py
+++ make.py
@@ -1,9 +1,11 @@
 """Makefile-like"""
+import os
+import shutil
 import shlex
 import subprocess
 import sys
 from pathlib import Path
-from typing import List, Sequence
+from typing import Dict, List, Optional, Sequence
 
 import typer
 
@@ -17,6 +19,12 @@ PYTHON_SOURCES = [
     )
 ]
 ECHO_PREFIX = "[make] "
+PYTHON = sys.executable
+
+# Sphinx options
+SPHINX_SOURCE_DIR = "source"
+SPHINX_BUILD_DIR = "build"
+SPHINX_OPTS = ["-W", "--keep-going", "-T"]
 
 
 def info(msg: str) -> None:
@@ -27,11 +35,19 @@ def error(msg: str) -> None:
     typer.secho(f"{ECHO_PREFIX}ERROR: {msg}", fg="red")
 
 
-def run_command(cmd: Sequence[str], *, check: bool = True) -> subprocess.CompletedProcess[bytes]:
+def run_command(
+    cmd: Sequence[str],
+    *,
+    check: bool = True,
+    env: Optional[Dict[str, str]] = None,
+    cwd: Path = REPO_DIR,
+) -> subprocess.CompletedProcess[bytes]:
     info(f"Running command: {shlex.join(cmd)}")
-    proc = subprocess.run(cmd, check=False)
-    if check:
-        proc.check_returncode()
+    env = {**os.environ, **(env or {})}
+    proc = subprocess.run(cmd, check=False, env=env, cwd=cwd)
+    if check and proc.returncode != 0:
+        error(f"Command returned non-zero exit status {proc.returncode}.")
+        sys.exit(1)
     return proc
 
 
@@ -39,7 +55,19 @@ cli = typer.Typer()
 
 
 @cli.command()
+def edit() -> None:
+    """Launch Visual Studio Code."""
+    code_exe = shutil.which("code")
+    if not code_exe:
+        error("Could not find 'code' in PATH.")
+        sys.exit(1)
+    env = {"PYTHON": PYTHON}  # for "python.pythonPath" in .vscode/settings.json
+    run_command([code_exe, str(REPO_DIR)], env=env)
+
+
+@cli.command()
 def lint() -> None:
+    """Run linters."""
     failed: List[str] = []
     for cmd in (
         ["isort", "--check"] + PYTHON_SOURCES,
@@ -59,8 +87,24 @@ def lint() -> None:
 
 @cli.command()
 def format() -> None:  # pylint: disable=redefined-builtin
+    """Run auto-formatters."""
     run_command(["isort"] + PYTHON_SOURCES)
     run_command(["black"] + PYTHON_SOURCES)
+
+
+@cli.command()
+def build() -> None:
+    """Build blog content."""
+    run_command(
+        [
+            "sphinx-build",
+            SPHINX_SOURCE_DIR,
+            SPHINX_BUILD_DIR,
+            "-j",
+            "auto",
+        ]
+        + SPHINX_OPTS
+    )
 
 
 if __name__ == "__main__":
